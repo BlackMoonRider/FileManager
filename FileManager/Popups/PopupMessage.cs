75602028bcc19fb97a0dd00236191daa7c285b85
BlackMoonRider <theblackmoonrider@gmail.com>
2019-07-10 02:13:21 +0300

    Clean up PopupMessage

--- FileManager/Popups/PopupMessage.cs
+++ FileManager/Popups/PopupMessage.cs
@@ -9,22 +9,24 @@ namespace FileManager
     class PopupMessage : AbstractPopup
     {
         private PanelSet panelSet;
+        private string message;
 
-        public PopupMessage(PanelSet panelSet, string header = "Info") : base(header)
+        public PopupMessage(PanelSet panelSet, string message, string header = "Info") : base(header)
         {
             this.panelSet = panelSet;
+            this.message = message;
         }
 
         public override void Render()
         {
             base.Render();
 
-            var lines = header.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            var lines = message.Split(new string[] { "\r\n" }, StringSplitOptions.None);
 
             for (int i = 0; i < lines.Length; i++)
             {
                 Console.CursorTop = offsetY + i;
-                Console.CursorLeft = offsetX + 1;
+                Console.CursorLeft = offsetX;
                 Console.WriteLine(lines[i]);
             }
 
