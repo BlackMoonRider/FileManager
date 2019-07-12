3861570c1aff525c170e08b7fb3819e91da6db08
BlackMoonRider <theblackmoonrider@gmail.com>
2019-07-12 23:41:28 +0300

    FIx duplicate code in ShowProperties

--- FileManager/ActionPerformers/ShowProperties.cs
+++ FileManager/ActionPerformers/ShowProperties.cs
@@ -19,33 +19,26 @@ namespace FileManager.ActionPerformers
             string info = String.Empty;
             int readOnly = ((int)(sourceInfo.Attributes) & (int)FileAttributes.ReadOnly);
 
+            stringBuilder.AppendLine("Name:               " + sourceInfo.Name);
+            stringBuilder.AppendLine("Parent direcotry:   " + Path.GetDirectoryName(sourceInfo.FullName));
+            stringBuilder.AppendLine("Root direcotry:     " + Path.GetPathRoot(sourceInfo.FullName));
+            stringBuilder.AppendLine("Read-only:          " + ((readOnly == 1) ? "true" : "false"));
+            stringBuilder.AppendLine("Last read time:     " + sourceInfo.LastAccessTime);
+            stringBuilder.AppendLine("Last write time:    " + sourceInfo.LastWriteTime);
+
             if (sourceInfo is FileInfo fileInfo)
             {
-                stringBuilder.AppendLine("Name:               " + sourceInfo.Name);
-                stringBuilder.AppendLine("Parent direcotry:   " + Path.GetDirectoryName(sourceInfo.FullName));
-                stringBuilder.AppendLine("Root direcotry:     " + Path.GetPathRoot(sourceInfo.FullName));
-                stringBuilder.AppendLine("Read-only:          " + ((readOnly == 1) ? "true" : "false"));
-                stringBuilder.AppendLine("Last read time:     " + sourceInfo.LastAccessTime);
-                stringBuilder.AppendLine("Last write time:    " + sourceInfo.LastWriteTime);
                 stringBuilder.AppendLine("Size:               " + fileInfo.Length.PrintAsNormalizedSize());
-
-                info = stringBuilder.ToString();
             }
 
             else if (sourceInfo is DirectoryInfo directoryInfo)
             {
-                stringBuilder.AppendLine("Name:               " + sourceInfo.Name);
-                stringBuilder.AppendLine("Parent direcotry:   " + Path.GetDirectoryName(sourceInfo.FullName));
-                stringBuilder.AppendLine("Root direcotry:     " + Path.GetPathRoot(sourceInfo.FullName));
-                stringBuilder.AppendLine("Read-only:          " + ((readOnly == 1) ? "true" : "false"));
-                stringBuilder.AppendLine("Last read time:     " + sourceInfo.LastAccessTime);
-                stringBuilder.AppendLine("Last write time:    " + sourceInfo.LastWriteTime);
                 stringBuilder.AppendLine("Size:               " + directoryInfo.DirectorySize().PrintAsNormalizedSize());
                 stringBuilder.AppendLine("Files:              " + Directory.GetFiles(sourceInfo.FullName).Count());
                 stringBuilder.AppendLine("Folders:            " + Directory.GetDirectories(sourceInfo.FullName).Count());
-
-                info = stringBuilder.ToString();
             }
+
+            info = stringBuilder.ToString();
 
             PopupMessage popupMessage = new PopupMessage(panelSet, info, "Properties");
             popupMessage.Render();
